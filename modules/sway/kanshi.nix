33915ead9bdbcc8035155f3ece74c1cc35af9899
Danilo Cianfrone <danilocianfr@gmail.com>
2023-03-15 08:31:21 +0100

    feat: add new LG monitor to kanshi config

--- modules/sway/kanshi.nix
+++ modules/sway/kanshi.nix
@@ -10,6 +10,10 @@
           criteria = "eDP-1";
           scale = 1.0;
         }
+        {
+          criteria = "LG Electronics LG HDR 4K 211MAHUMY369";
+          scale = 1.25;
+        }
       ];
     };
 
