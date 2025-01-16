92e2a39c97b1eff7146740e8bbf29aa87177f3ea
Danilo Cianfrone <danilocianfr@gmail.com>
2025-01-16 21:03:52 +0100

    fix

--- hosts/momonoke/kubernetes.nix
+++ hosts/momonoke/kubernetes.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 let
   kubernetesHostname = "k8s.momonoke.ar3s3ru.dev";
 in
@@ -56,6 +56,6 @@ in
 
   # Disable limits for the number of open files by k3s containers,
   # or the telemetry stack will complain.
-  systemd.services.k3s.serviceConfig.LimitNOFILE = lib.mkForce "infinity";
-  systemd.services.k3s.serviceConfig.LimitNOFILESoft = lib.mkForce "infinity";
+  systemd.services.k3s.serviceConfig.LimitNOFILE = lib.mkIf config.services.k3s.enable (lib.mkForce "infinity");
+  systemd.services.k3s.serviceConfig.LimitNOFILESoft = lib.mkIf config.services.k3s.enable (lib.mkForce "infinity");
 }
