882237e6f57b588ff362a40505e262d7e3b5edac
Danilo Cianfrone <danilocianfr@gmail.com>
2025-01-19 21:48:01 +0100

    chore: remove last traces of terragrunt/terraform

--- flake.nix
+++ flake.nix
@@ -48,21 +48,10 @@
         devShells.default = with pkgs; mkShell {
           name = "default";
           packages = [
-            # Nice utilities.
             git
             unixtools.watch
             gnumake
-
-            # Relevant packages to apply the configurations.
-            terraform
-            terragrunt
-
-            # Linters
             nil
-
-            # Temporarily for Kubernetes
-            kubectl
-            k9s
           ];
         };
       }));
